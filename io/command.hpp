e8f56de6a94fac607ab8f6f7af4b873a32566111
nanye1 <s420883290@126.com>
2026-05-06 21:48:39 +0800

    Update command.hpp

--- io/command.hpp
+++ io/command.hpp
@@ -9,7 +9,10 @@ struct Command
   bool shoot;
   double yaw;
   double pitch;
-  double horizon_distance = 0;  //无人机专有
+  double horizon_distance = 0;  // Legacy CAN path uses horizontal distance.
+  int armor_id = 0;
+  double vx = 0;
+  double vy = 0;
   double big_yaw = 0;
   double small_yaw = 0;
   bool has_target_yaw = false;
